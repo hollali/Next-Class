3949fe4b0e5505b0a1d6389d03ee824fee40ad1c
Techwithmax <bigfish3656@gmail.com>
2025-08-30 16:26:53 +0000

    m

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
     compileSdk = 35
 
     defaultConfig {
-        applicationId = "com.ahofama.nextclass"
+        applicationId = "com.ahofama.nextclass.v2"
         minSdk = 28
         targetSdk = 35
         versionCode = 1
